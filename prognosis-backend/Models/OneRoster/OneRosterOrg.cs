3800771ce51c881aebe7a18ccffe49179568b7ed
Dylan Howard <dylanhoward6@gamil.com>
2024-07-30 08:42:17 -0500

    Regrouped OneRoster classes

--- prognosis-backend/Models/OneRoster/OneRosterOrg.cs
+++ prognosis-backend/Models/OneRoster/OneRosterOrg.cs
@@ -10,42 +10,54 @@ namespace prognosis_backend.models
         [JsonPropertyName("sourcedId")]
         public Guid SourcedId { get; set; }
         [JsonPropertyName("status")]
-        public string? Status { get; set; }
+        public required string Status { get; set; }
         [JsonPropertyName("dateLastModified")]
         public DateTime DateLastModified { get; set; }
         [JsonPropertyName("metadata")]
         public OneRosterOrgMetadata? Metadata { get; set; }
         [JsonPropertyName("name")]
-        public string? Name { get; set; }
+        public required string Name { get; set; }
         [JsonPropertyName("identifier")]
-        public string? Identifier { get; set; }
+        public required string Identifier { get; set; }
         [JsonPropertyName("type")]
         public string? Type { get; set; }
-        [Column("address")]
-        public string? Address { get; set; }
-        [Column("city")]
-        public string? City { get; set; }
-        [Column("state")]
-        public string? State { get; set; }
-        [Column("zip")]
-        public string? Zip { get; set; }
 
         public static implicit operator string?(OneRosterOrg? v)
         {
             if (v == null) {
-              return null;
+                return null;
             }
 
             string objString = $"{{\n" +
-              $"\tSourcedId: {v.SourcedId},\n" +
-              $"\tStatus: {v.Status},\n" +
-              $"\tDateLastModified: {v.DateLastModified},\n" +
-              $"\tName: {v.Name},\n" +
-              $"\tIdentifier: {v.Identifier},\n" +
-              $"\tType: {v.Type},\n" +
-            "}}";
+                $"\tSourcedId: {v.SourcedId},\n" +
+                $"\tStatus: {v.Status},\n" +
+                $"\tDateLastModified: {v.DateLastModified},\n" +
+                $"\tName: {v.Name},\n" +
+                $"\tIdentifier: {v.Identifier},\n" +
+                $"\tType: {v.Type},\n" +
+                "}}";
 
             return objString;
+        }
+
+        public static implicit operator Org?(OneRosterOrg? v)
+        {
+            if (v == null) {
+                return null;
+            }
+
+            return new Org {
+                SourcedId = v.SourcedId,
+                Status = v.Status == "active",
+                DateLastModified = v.DateLastModified,
+                Name = v.Name,
+                Identifier = v.Identifier,
+                Type = v.Type,
+                Address = v.Metadata?.Address?.FirstLine,
+                City = v.Metadata?.Address?.City,
+                State = v.Metadata?.Address?.State,
+                Zip = v.Metadata?.Address?.ZipCode,
+            };
         }
     }
     public class OneRosterOrgAddress {
