2419d313a85a6d297029405957b7728e51147a6a
Dylan Howard <Dylan Howard>
2024-08-28 15:44:49 -0500

    Adjusted dashboard styling

--- prognosis-frontend/src/Modules/Chart.tsx
+++ prognosis-frontend/src/Modules/Chart.tsx
@@ -17,22 +17,25 @@ type DataPoint = {
 };
 
 export const Chart = ({ title, data } : { title: string, data: DataPoint[] }) => {
+  const chartData = data.map(({ name, count }) => ({
+    name: name.substring(0,name.lastIndexOf('/')) + ' ' + name.substring(name.indexOf(' ')),
+    count,
+  }));
+
   return (
-    <Paper sx={{
-      p: 2,
-    }}>
+    <Paper sx={{ p: 2 }}>
       <Typography variant="body1" fontSize={18} fontWeight={600} sx={{ mb: 2 }}>{`${title}`}</Typography>
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart width={500} height={300} data={data}>
+        <LineChart width={500} height={300} data={chartData}>
           <XAxis dataKey="name"/>
-          <YAxis/>
+          <YAxis domain={['dataMin - 10000', 'dataMax + 10000']} />
           <Tooltip />
-          <Legend />
-          <CartesianGrid strokeDasharray="5 5"/>
+          {/* <Legend payload={[ { value: 'Records' }]} iconType="square" /> */}
+          <Legend payload={[ { value: 'Records' }]} iconType="square" />
+          <CartesianGrid strokeDasharray="5 5" vertical={false} />
           <Line type="monotone" dataKey="count" stroke="#8884d8" />
         </LineChart>
       </ResponsiveContainer>
-      
     </Paper>
   );
 }
