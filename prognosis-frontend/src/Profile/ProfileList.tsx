ea0e3e6fef7ea0ca4fd43ec384e03ea558c1298f
Dylan Howard <dylanhoward6@gamil.com>
2024-08-30 16:09:35 -0500

    Added Container for max-width on Profiles List

--- prognosis-frontend/src/Profile/ProfileList.tsx
+++ prognosis-frontend/src/Profile/ProfileList.tsx
@@ -9,6 +9,7 @@ import {
   TextField
 } from 'react-admin';
 import QuickFilter from '../Modules/QuickFilter';
+import Container from '@mui/material/Container';
 
 const profileFilters = [
   <SearchInput source="q" alwaysOn />,
@@ -19,15 +20,17 @@ const profileFilters = [
 ];
 
 export const ProfileList = () => (
-  <List filters={profileFilters}>
-    <Datagrid rowClick="show">
-      <EmailField source="email" label="Email" />
-      <TextField source="firstName" label="First" />
-      <TextField source="lastName" label="Last" />
-      <BooleanField source="status" label="Status" />
-      <BooleanField source="claimed" label="Claimed" />
-      <BooleanField source="locked" label="Locked" />
-      <TextField source="mfaMethod" label="Mfa Method" />
-    </Datagrid>
-  </List>
+  <Container maxWidth="xl">
+    <List filters={profileFilters}>
+      <Datagrid rowClick="show">
+        <EmailField source="email" label="Email" />
+        <TextField source="firstName" label="First" />
+        <TextField source="lastName" label="Last" />
+        <BooleanField source="status" label="Status" />
+        <BooleanField source="claimed" label="Claimed" />
+        <BooleanField source="locked" label="Locked" />
+        <TextField source="mfaMethod" label="Mfa Method" />
+      </Datagrid>
+    </List>
+  </Container>
 );
