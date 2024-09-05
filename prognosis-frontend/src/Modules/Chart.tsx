1235de0972442ea339308b039f466ac3b874d944
Dylan Howard <dylanhoward6@gamil.com>
2024-09-05 13:23:08 -0500

    Fixed y-axis range bug

--- prognosis-frontend/src/Modules/Chart.tsx
+++ prognosis-frontend/src/Modules/Chart.tsx
@@ -28,9 +28,8 @@ export const Chart = ({ title, data } : { title: string, data: DataPoint[] }) =>
       <ResponsiveContainer width="100%" height={300}>
         <LineChart width={500} height={300} data={chartData}>
           <XAxis dataKey="name"/>
-          <YAxis domain={['dataMin - 10000', 'dataMax + 10000']} />
+          <YAxis domain={[(dataMin: any) => Math.max(0, dataMin - 10000), 'dataMax + 10000']} />
           <Tooltip />
-          {/* <Legend payload={[ { value: 'Records' }]} iconType="square" /> */}
           <Legend payload={[ { value: 'Records' }]} iconType="square" />
           <CartesianGrid strokeDasharray="5 5" vertical={false} />
           <Line type="monotone" dataKey="count" stroke="#8884d8" />
